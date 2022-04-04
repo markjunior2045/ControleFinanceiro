4ae72a946eb4b44a88cdca41b4db1ed85199f81a
Lucas00Caires <lucas00caires@gmail.com>
2022-04-04 15:03:40 -0300

    Criada tela cadastro de conta corrente em HTML/CSS

---
+++ FrontEnd/src/app/cadastro-conta-corrente/cadastro-conta-corrente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-conta-corrente',
+  templateUrl: './cadastro-conta-corrente.component.html',
+  styleUrls: ['./cadastro-conta-corrente.component.css']
+})
+export class CadastroContaCorrenteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
